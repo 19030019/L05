b757d15a850ff21781717cea94f48d1835fdb877
anisaaffendi <19030019@rp.edu.sg>
2020-11-28 10:32:52 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {MovieList} from './Movies.js';
+import BoatsList from './Boats.js';
+
+import {
+    View,
+    Text,
+    Image,
+    ScrollView
+} from 'react-native';
+
+const App: () => React$Node =() => {
+    return (
+        <ScrollView>
+            <Text>Lesson 05 Exercise</Text>
+            <MovieList/>
+            <Text>Get A Boat - For Sale</Text>
+            <BoatsList/>
+        </ScrollView>
+    );
+};
+
+export default App;
---
+++ Movies.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {
+    View,
+    Text,
+    Image,
+    ScrollView
+} from 'react-native';
+
+const movie = {
+    title: 'Doctor Sleep',
+    year: '2019'
+};
+
+const MOVIES_DATA = [
+    {
+        title: 'Doctor Sleep',
+        year: '2019',
+        poster: require('./img/doctor-sleep.jpg'),
+    },
+    {
+        title: 'Midway',
+        year: '2019',
+        poster: require('./img/midway.jpg'),
+    },
+}
+]
+
+const MovieList = () => {
+  return MOVIES_DATA.map((movie) => {
+        return <Movie title={movie.title} year={movie.year} poster={movie.poster}/>;
+  }
+  )}
+
+
+class Movie extends React.Component {
+  render() {
+    return (
+        <View>
+          <Text>
+            {this.props.title}
+            {'\n'}
+            {this.props.year}
+          </Text>
+          <Image source={this.props.poster}/>
+        </View>
+    );
+  }
+}
+
+export default MovieList;
